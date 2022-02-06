4832f7ed88e657c58a8ad149077a25e8ecd6d389
tmf9047 <tmf9047@rit.edu>
2022-02-06 00:01:00 -0500

    added Chat Now button

--- src/project/main.py
+++ src/project/main.py
@@ -12,3 +12,8 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/chat')
+@login_required
+def chat():
+    return render_template('chat.html')
