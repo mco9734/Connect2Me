75a671e715ada312929467b842b0ca1e35f2a825
mco9734 <mco9734@rit.edu>
2022-02-05 20:21:22 -0500

    signup

--- src/project/auth.py
+++ src/project/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -10,6 +12,28 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
